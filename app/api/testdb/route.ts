115dd806fb02230ae0576ed2dddea6fdebbf1180
Aisha <aishakwatra3@gmail.com>
2025-11-11 20:26:46 +0700

    Supabase connected and successful data retrieval

---
+++ app/api/testdb/route.ts
@@ -0,0 +1,19 @@
+import { NextRequest, NextResponse } from "next/server";
+
+import { createClient } from "../../../lib/supabase/server";
+import { cookies } from "next/headers";
+
+export async function GET(req:NextRequest){
+    const cookieStore = await cookies();
+    const supabase = createClient(cookieStore);
+
+    const {data: allProjects, error } = await supabase. from ('projects')
+        .select(`*`);
+
+    if (error){
+        console.log("Error fetching all projects: ", error);
+        return NextResponse.json({error: error.message}, {status:500});
+    }
+    return NextResponse.json(allProjects);
+
+}
