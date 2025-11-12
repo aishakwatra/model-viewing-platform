5f5407dd10f98b5ae0769ebad05997ac84c2f5cb
Aisha <aishakwatra3@gmail.com>
2025-11-12 14:41:32 +0700

    fix cookie error

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -3,7 +3,6 @@ import { createServerClient, type CookieOptions } from "@supabase/ssr";
 import { cookies } from "next/headers";
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
 export const createClient = (cookieStore: ReturnType<typeof cookies>) => {
